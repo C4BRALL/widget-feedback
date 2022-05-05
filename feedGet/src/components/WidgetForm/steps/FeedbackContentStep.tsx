0ea36186da4f561d815fbe6db1bf940af999b760
Cabral <jcabral304@gmail.com>
2022-05-05 12:41:42 -0300

    [ScreenshotButton] has been implemented

--- feedGet/src/components/WidgetForm/steps/FeedbackContentStep.tsx
+++ feedGet/src/components/WidgetForm/steps/FeedbackContentStep.tsx
@@ -1,6 +1,8 @@
 import { ArrowLeft } from "phosphor-react";
+import { useState } from "react";
 import { FeedbackType, feedbackTypes } from "..";
 import { CloseButton } from "../../CloseButton";
+import { ScreenshotButton } from "../ScreenshotButton";
 
 interface FeedbackContentStepProps {
   feedbackType: FeedbackType;
@@ -10,7 +12,9 @@ interface FeedbackContentStepProps {
 export function FeedbackContentStep({
   feedbackType,
   onFeedbackRestart,
-  }: FeedbackContentStepProps) {
+}: FeedbackContentStepProps) {
+  const [screenshot, setScreenshot] = useState<string | null>(null);
+
   const feedbackTypeInfo = feedbackTypes[feedbackType]
 
   return (
@@ -38,9 +42,22 @@ export function FeedbackContentStep({
 
       <form className="my-4 w-full">
         <textarea
-          className=""
+          className="min-w-[304px] w-full min-h-[112px] text-sm placeholder-zinc-100 border-zinc-600 bg-transparent rounded-md focus:border-brand-500 focus:ring-brand-500 focus:ring-1 focus:outline-none resize-none scrollbar scrollbar-thumb-zinc-700 scrollbar-track-transparent scrollbar-thin"
           placeholder="Conte com detalhes o que está acontecendo..."
         />
+
+        <footer className="flex gap-2 mt-2">
+          <ScreenshotButton
+            screenshot={ screenshot }
+            onScreenshotTook={ setScreenshot }
+          />
+          <button
+            type="submit"
+            className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500"
+          >
+            Enviar Feedback
+          </button>
+        </footer>
       </form>
     </>
   );
