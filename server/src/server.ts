47a6aee242ef88194518cae52a09f8bb8bcafc95
Cabral <jcabral304@gmail.com>
2022-05-07 10:51:23 -0300

    back-end has been done

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,13 @@
 import express from 'express';
+import cors from 'cors';
 import { routes } from './routes';
 
 const app = express();
 const port = 3333;
+
+app.use(cors({
+  origin: 'http://localhost:3000'
+}));
 
 app.use(express.json());
 
